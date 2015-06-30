25294e01d3ca40743889eefab56b1654a99f30cd
Andrei Tognolo <andreitognolo@gmail.com>
2015-06-30 13:37:43 -0300

    Update initialize-android.sh

--- initialize-android.sh
+++ initialize-android.sh
@@ -10,14 +10,14 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # fetch and initialize $ANDROID_HOME
   download-android
   # Use the latest android sdk tools
-  echo y | android update sdk --no-ui --filter platform-tools > /dev/null
-  echo y | android update sdk --no-ui --filter tools > /dev/null
+  echo y | android update sdk --no-ui -a platform-tools > /dev/null
+  echo y | android update sdk --no-ui -a tools > /dev/null
 
   # The BuildTools version used by your project
-  echo y | android update sdk --no-ui --filter build-tools-20.0.0 --all > /dev/null
+  echo y | android update sdk --no-ui -a build-tools-20.0.0 --all > /dev/null
 
   # The SDK version used to compile your project
-  echo y | android update sdk --no-ui --filter android-18 > /dev/null
+  echo y | android update sdk --no-ui -a android-18 > /dev/null
 
   # uncomment to install the Extra/Android Support Library
   # echo y | android update sdk --no-ui --filter extra-android-support --all > /dev/null
@@ -27,7 +27,7 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # echo y | android update sdk --no-ui --filter extra-android-m2repository --all > /dev/null
 
   # Specify at least one system image if you want to run emulator tests
-  echo y | android update sdk --no-ui --filter sys-img-armeabi-v7a-android-19 --all > /dev/null
-  ...
+  echo y | android update sdk --no-ui -a sys-img-armeabi-v7a-android-19 --all > /dev/null
+  
   touch ${INITIALIZATION_FILE}
 fi
